833347d57a77a55baf002f6e1ea6d672083aa909
Burcu Deniz <dnzzburcu@gmail.com>
2023-07-26 13:54:47 +0300

    wall class completed.

--- src/Main.java
+++ src/Main.java
@@ -12,7 +12,7 @@ public class Main {
         Wall wall = new Wall(5, 4);
         System.out.println("area= " + wall.getArea());
         wall.setHeight(-1.5);
-        wall.setWidth(1.5);
+        wall.setWidth(2.5);
         System.out.println("width= " + wall.getWidth());
         System.out.println("height= " + wall.getHeight());
         System.out.println("area= " + wall.getArea());
